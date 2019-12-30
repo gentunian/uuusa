7efca43c6149025e2a350893654520d4531e72d1
Sebastián Treu <sebastian.treu@gmail.com>
2019-12-30 12:50:21 -0300

    profile list

--- src/main/java/org/grupolys/profiles/ProfileCreator.java
+++ src/main/java/org/grupolys/profiles/ProfileCreator.java
@@ -2,15 +2,15 @@ package org.grupolys.profiles;
 
 import org.grupolys.profiles.exception.ProfileNotFoundException;
 
-public interface ProfileCreator {
+interface ProfileCreator {
     /**
      * Stores profile information with name {@literal}profileName.
      * 
      * @param profileName the name of the profile. This is useful to name the
      *                    profile.
-     * @param Profile     the profile to store.
+     * @param profile     the profile to store.
      */
-    public boolean saveProfile(String profileName, Profile profile);
+    boolean saveProfile(String profileName, Profile profile);
 
     /**
      * Loads profile information for a profile named {@literal}profileName.
@@ -18,5 +18,10 @@ public interface ProfileCreator {
      * @param profileName the name of the profile to load
      * @return the loaded Profile (null if profile does not exist)
      */
-    public Profile loadProfile(String profileName) throws ProfileNotFoundException;
+    Profile loadProfile(String profileName) throws ProfileNotFoundException;
+
+    /**
+     * @return list of saved profiles
+     */
+    String[] profiles();
 }
