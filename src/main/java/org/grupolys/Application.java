c634be2f11ac9a5d329442062a8f184436149268
Sebastián Treu <sebastian.treu@gmail.com>
2019-12-30 13:00:47 -0300

    simplify init

--- src/main/java/org/grupolys/Application.java
+++ src/main/java/org/grupolys/Application.java
@@ -7,18 +7,17 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import com.hazelcast.config.Config;
-import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
 
+import org.grupolys.hazelcast.HazelcastListener;
 import org.grupolys.profiles.FilesystemProfileCreator;
 import org.grupolys.profiles.Profile;
 import org.grupolys.profiles.exception.ProfileNotFoundException;
 import org.grupolys.samulan.Samulan;
-import org.grupolys.spring.dao.utils.Utils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.impl.Arguments;
@@ -30,10 +29,11 @@ public class Application {
 
     public static void main(String[] args) throws FileNotFoundException {
         if (springboot(args)) {
-            initDataFromFilesystem(FilesystemProfileCreator.PROFILES_DIR);
-            Utils.getProcessorInstance();
-            Utils.getRules();
-            SpringApplication.run(Application.class, args);
+            ApplicationContext context = SpringApplication.run(Application.class, args);
+            // HazelcastInitializer hzi = context.getBean(HazelcastInitializer.class);
+            // initDataFromFilesystem(hz, FilesystemProfileCreator.PROFILES_DIR);
+            // Utils.getProcessorInstance();
+            // Utils.getRules();
         } else {
             Samulan.main(args);
         }
@@ -51,39 +51,38 @@ public class Application {
         }
     }
 
-    private static void initDataFromFilesystem(String directory) {
-        Config cfg = new Config(System.getenv("HOSTNAME"));
-        HazelcastInstance hz = Hazelcast.newHazelcastInstance(cfg);
-        IMap<String, Profile> map = hz.getMap("profileData");
-        map.addEntryListener(new Utils.MapListener(), true);
+//    private static void initDataFromFilesystem(HazelcastInstance hz, String directory) {
+        // Config cfg = new Config(System.getenv("HOSTNAME"));
+        // HazelcastInstance hz = Hazelcast.newHazelcastInstance(cfg);
+//        IMap<String, Profile> map = hz.getMap("profileData");
+//        map.addEntryListener(new HazelcastListener(), true);
 
-        FilesystemProfileCreator fpc = new FilesystemProfileCreator();
+//        FilesystemProfileCreator fpc = new FilesystemProfileCreator();
 
         // FilesystemProfileCreator.PROFILES_DIR;
-        try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(Paths.get(directory))) {
-            for (Path path : directoryStream) {
-                String profileName = path.getFileName().toString();
-                if (!map.containsKey(profileName)) {
-                    map.lock(profileName);
-                    try {
-                        Profile profile = fpc.loadProfile(profileName);
-                        map.put(profileName, profile);
-                    } catch (ProfileNotFoundException e) {
-                        e.printStackTrace();
-                    } finally {
-                        map.unlock(profileName);
-                    }
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(Paths.get(directory))) {
+//            for (Path path : directoryStream) {
+//                String profileName = path.getFileName().toString();
+//                if (!map.containsKey(profileName)) {
+//                    map.lock(profileName);
+//                    try {
+//                        Profile profile = fpc.loadProfile(profileName);
+//                        map.put(profileName, profile);
+//                    } catch (ProfileNotFoundException e) {
+//                        e.printStackTrace();
+//                    } finally {
+//                        map.unlock(profileName);
+//                    }
+//                }
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
         // RuleBasedAnalyser rba = prepare(fspc.loadProfile(profileName));
         // Processor processor = prepareProcessor("/opt/uuusa/data/profiles/" +
         // profileName);
         // Map<String, RuleBasedAnalyser> rulesPerProfile;
 
         // u.set("profile", rba);
-    }
-
+//    }
 }
